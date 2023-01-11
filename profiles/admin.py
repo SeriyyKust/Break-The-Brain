492057169af8696c4a4155f3e1804aa5bccf2c86
Timofey Zavrazhnov <tima.zavrazhnov@mail.ru>
2023-01-12 00:07:59 +0300

    Create admin page

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Profile, Point
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'date_birth', 'photo', 'quote')
+    list_display_links = ('user_id', )
+    search_fields = ('user_id', )
+    list_editable = ('date_birth', 'photo', 'quote')
+
+
+class PointAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'number_points')
+    list_display_links = ('user_id', )
+    search_fields = ('user_id', )
+    list_editable = ('number_points', )
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Point, PointAdmin)
