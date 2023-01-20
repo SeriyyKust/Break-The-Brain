3fcedf04c371c4b70ba141e5680db523a44049bd
Timofey Zavrazhnov <tima.zavrazhnov@mail.ru>
2023-01-20 14:38:13 +0300

    Create Quizzes List and Datail views

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -1,1 +1,15 @@
-urlpatterns = []
+from django.urls import path
+from breakthebrain import settings
+from django.conf.urls.static import static
+from .views import QuizzesListView, QuizzesDetailView
+
+
+urlpatterns = [
+    path('all_quizzes/', QuizzesListView.as_view(), name="all_quizzes"),
+    path('detail_quizzes/<slug:task_slug>/', QuizzesDetailView.as_view(), name="detail_quizzes"),
+
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- quizzes/views.py
+++ quizzes/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from profiles.utils import DataMixin
+from .models import Task
 
-# Create your views here.
+
+class QuizzesListView(DataMixin, ListView):
+    model = Task
+    template_name = "quizzes/all_tasks.html"
+    context_object_name = "tasks"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Все задания"
+        return context | self.get_user_context()
+
+
+class QuizzesDetailView(DataMixin, DetailView):
+    model = Task
+    template_name = "quizzes/detail_task.html"
+    context_object_name = "task"
+    slug_url_kwarg = "task_slug"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = context["task"]
+        return context | self.get_user_context()
